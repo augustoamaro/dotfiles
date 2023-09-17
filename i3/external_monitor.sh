5d8b2dc0556ca8b39548c00db5b47fc34df43243
Leônidas Augusto Amaro <augustoamaro@proton.me>
2023-09-16 23:41:33 -0300

    Update external_monitor.sh

--- i3/external_monitor.sh
+++ i3/external_monitor.sh
@@ -3,4 +3,9 @@
 #xrandr --output eDP-1 --auto --output HDMI-1 --auto --above eDP-1
 
 # Two monitors
-xrandr --output eDP-1 --auto --output HDMI-1 --auto --left-of eDP-1 --output DP-1 --auto --above HDMI-1
+#xrandr --output eDP-1 --auto --output HDMI-1 --auto --left-of DP-1 --output HDMI-1 --auto --above DP-1
+#xrandr --output eDP-1 --auto --output DP-1 --auto --left-of eDP-1 --output HDMI-1 --auto --above DP-1
+
+xrandr --output eDP-1 --primary --auto \
+       --output HDMI-1 --auto --above DP-1 \
+       --output DP-1 --auto --left-of eDP-1
